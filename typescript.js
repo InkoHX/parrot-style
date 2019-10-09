f48cc57402b50575619a26616f4317c12bc8d3cb
InkoHX <inkohx@gmail.com>
2019-10-09 11:51:10 +0900

    Add TypeScript rules.

---
+++ typescript.js
@@ -0,0 +1,53 @@
+module.exports = {
+  extends: [
+    './.eslintrc.json',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint'
+  ],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'brace-style': 'off',
+        'func-call-spacing': 'off',
+        'indent': 'off',
+        'no-extra-parens': 'off',
+        'quotes': 'off',
+        'semi': 'off',
+        '@typescript-eslint/brace-style': ['error'],
+        '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+        '@typescript-eslint/explicit-member-accessibility': 'error',
+        '@typescript-eslint/func-call-spacing': 'error',
+        '@typescript-eslint/generic-type-naming': 'error',
+        '@typescript-eslint/indent': ['error', 2],
+        '@typescript-eslint/member-delimiter-style': [
+          'error',
+          {
+            'multiline': {
+              'delimiter': 'comma',
+              'requireLast': false
+            },
+            'singleline': {
+              'delimiter': 'comma',
+              'requireLast': false
+            }
+          }
+        ],
+        '@typescript-eslint/member-naming': 'error',
+        '@typescript-eslint/no-extra-parens': 'error',
+        '@typescript-eslint/no-floating-promises': 'error',
+        '@typescript-eslint/no-parameter-properties': 'error',
+        '@typescript-eslint/no-require-imports': 'error',
+        '@typescript-eslint/no-useless-constructor': 'error',
+        '@typescript-eslint/prefer-for-of': 'error',
+        '@typescript-eslint/prefer-readonly': 'error',
+        '@typescript-eslint/quotes': 'error',
+        '@typescript-eslint/semi': ['error', 'never']
+      }
+    }
+  ]
+}
