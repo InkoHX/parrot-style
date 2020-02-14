980a561935540835bbf4ca55a2b62b18fe66d6d4
InkoHX <inkohx@gmail.com>
2020-02-14 11:50:07 +0900

    Add react.js

---
+++ react.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    'jsx-a11y'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  extends: [
+    './eslintrc.json',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/strict'
+  ],
+  rules: {
+    'react/jsx-indent': ['error', 2, { 'checkAttributes': true, 'indentLogicalExpressions': true }],
+    'react/jsx-indent-props': ['error', 2],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
+    
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        './typescript.js'
+      ]
+    }
+  ]
+}
