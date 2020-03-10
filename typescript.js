3318ec9ba8a42490cbe1854a382c2bd3c288ce09
InkoHX <inkohx@gmail.com>
2020-03-10 13:03:09 +0900

    Update typescript.js

--- typescript.js
+++ typescript.js
@@ -12,6 +12,7 @@ module.exports = {
     {
       files: ['*.ts', '*.tsx'],
       rules: {
+        'no-undef': 'off',
         'brace-style': 'off',
         'func-call-spacing': 'off',
         'indent': 'off',
@@ -19,6 +20,32 @@ module.exports = {
         'quotes': 'off',
         'semi': 'off',
         'no-unused-vars': 'off',
+        'default-param-last': 'off',
+        'no-use-before-define': 'off',
+        'camelcase': 'off',
+        '@typescript-eslint/no-use-before-define': ['error', {
+          functions: false,
+          classes: false,
+          enums: false,
+          variables: false,
+          typedefs: false
+        }],
+        '@typescript-eslint/camelcase': ['error', {
+          properties: 'never',
+          genericType: 'always'
+        }],
+        '@typescript-eslint/no-empty-interface': 'error',
+        '@typescript-eslint/no-unsafe-call': 'error',
+        '@typescript-eslint/no-unsafe-member-access': 'error',
+        '@typescript-eslint/no-unsafe-return': 'error',
+        '@typescript-eslint/consistent-type-assertions': [
+          'error',
+          {
+            assertionStyle: 'as',
+            objectLiteralTypeAssertions: 'never'
+          }
+        ],
+        '@typescript-eslint/array-type': ['error', { default: 'array-simple' }],
         '@typescript-eslint/brace-style': ['error'],
         '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
         '@typescript-eslint/explicit-member-accessibility': 'error',
@@ -43,6 +70,11 @@ module.exports = {
         '@typescript-eslint/no-parameter-properties': 'error',
         '@typescript-eslint/no-require-imports': 'error',
         '@typescript-eslint/no-useless-constructor': 'error',
+        '@typescript-eslint/prefer-function-type': 'error',
+        '@typescript-eslint/prefer-nullish-coalescing': 'error',
+        '@typescript-eslint/prefer-optional-chain': 'error',
+        '@typescript-eslint/prefer-readonly-parameter-types': 'error',
+        '@typescript-eslint/no-namespace': 'error',
         '@typescript-eslint/prefer-for-of': 'error',
         '@typescript-eslint/prefer-readonly': 'error',
         '@typescript-eslint/quotes': ['error', 'single'],
