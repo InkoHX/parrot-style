584b9eee46cc32d283d41828abfd8652db597303
InkoHX <inkohx@gmail.com>
2020-03-10 13:27:02 +0900

    Remove(rule): prefer-readonly-parameter-types

--- typescript.js
+++ typescript.js
@@ -79,7 +79,6 @@ module.exports = {
         '@typescript-eslint/prefer-function-type': 'error',
         '@typescript-eslint/prefer-nullish-coalescing': 'error',
         '@typescript-eslint/prefer-optional-chain': 'error',
-        '@typescript-eslint/prefer-readonly-parameter-types': 'error',
         '@typescript-eslint/no-namespace': 'error',
         '@typescript-eslint/prefer-for-of': 'error',
         '@typescript-eslint/prefer-readonly': 'error',
